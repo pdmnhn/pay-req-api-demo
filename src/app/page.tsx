ee4fa90f41db52766fba9f7af22a21e2fc3bb17a
Padmanabhan K <dev.pdmnhn@gmail.com>
2024-08-18 19:41:27 +0530

    Add demo order payment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,139 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+
+const googlePaymentDataRequest = {
+  environment: "TEST",
+  apiVersion: 2,
+  apiVersionMinor: 0,
+  merchantInfo: {
+    merchantName: "Example Merchant",
+  },
+  allowedPaymentMethods: [
+    {
+      type: "CARD",
+      parameters: {
+        allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+        allowedCardNetworks: [
+          "AMEX",
+          "DISCOVER",
+          "INTERAC",
+          "JCB",
+          "MASTERCARD",
+          "VISA",
+        ],
+      },
+      tokenizationSpecification: {
+        type: "PAYMENT_GATEWAY",
+        parameters: {
+          gateway: "example",
+          gatewayMerchantId: "exampleGatewayMerchantId",
+        },
+      },
+    },
+  ],
+};
+
+function buildSupportedPaymentMethodData() {
+  return [
+    {
+      supportedMethods: "basic-card",
+      data: {
+        supportedNetworks: [
+          "visa",
+          "mastercard",
+          "amex",
+          "jcb",
+          "diners",
+          "discover",
+          "mir",
+          "unionpay",
+        ],
+      },
+    },
+    {
+      supportedMethods: "https://google.com/pay",
+      data: googlePaymentDataRequest,
+    },
+  ];
+}
+
+function buildShoppingCartDetails() {
+  return {
+    id: "order-123",
+    displayItems: [
+      {
+        label: "Example item",
+        amount: { currency: "USD", value: "1.00" },
+      },
+    ],
+    total: {
+      label: "Total",
+      amount: { currency: "USD", value: "1.00" },
+    },
+  };
+}
 
 export default function Home() {
+  const [paymentRequest, setPaymentRequest] = useState<PaymentRequest>();
+  useEffect(() => {
+    const request = new PaymentRequest(
+      buildSupportedPaymentMethodData(),
+      buildShoppingCartDetails()
+    );
+    setPaymentRequest(request);
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <AppBar position="static">
+        <Toolbar variant="regular">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Payment Request API Demo by pdmnhn
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexGrow: 1,
+        }}
+      >
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              Payment Request API Demo
+            </Typography>
+            <Typography variant="body1">
+              Added Google Pay as one of the supported methods
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="medium" onClick={() => paymentRequest?.show()}>
+              Pay Now
+            </Button>
+          </CardActions>
+        </Card>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
